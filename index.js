0522b884afa9d75d66a0bcf6b04088622fdf1a58
srafi1 <shakilrafi0@gmail.com>
2016-12-29 22:45:14 -0500

    Commiting with gpush

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ var clients = {};
 io.on('connection', function(socket) {
     
     socket.on('joinRoom', function(room) {
-	console.log(socket.id + ' joined');
 	var num = room.roomnum;
 	if (!roomdata[num]) {
 	    roomdata[num] = [];
@@ -27,6 +26,7 @@ io.on('connection', function(socket) {
 	    }
 	}
 	clients[socket.id] = num;
+	console.log(socket.id + ' joined room ' + num);
     });
 
     socket.on('msg', function(msg) {
@@ -37,4 +37,21 @@ io.on('connection', function(socket) {
 	}
     });
     
+    socket.on('disconnect', function() {
+	console.log(socket.id + ' disconnected');
+	var removeRoom = true;
+	var num = clients[socket.id];
+	delete clients[socket.id];
+	for (var client in clients) {
+	    if (clients[client] == num) {
+		removeRoom = false;
+		break;
+	    }
+	}
+	if (removeRoom) {
+	    delete roomdata[num];
+	    console.log('room ' + num + ' deleted');
+	}
+    });
+
 });
